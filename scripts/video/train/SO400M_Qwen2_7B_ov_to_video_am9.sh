433bcdf4c4382333ee88b8f72a005f3e47049fd4
hyperfraise <damien.menigaux@veesion.io>
2025-07-02 13:22:01 +0200

    Remove --eval_dataset_size argument to fix training start

--- scripts/video/train/SO400M_Qwen2_7B_ov_to_video_am9.sh
+++ scripts/video/train/SO400M_Qwen2_7B_ov_to_video_am9.sh
@@ -114,7 +114,6 @@ ACCELERATE_CPU_AFFINITY=1 torchrun \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
     --eval_steps 1000 \
-    --eval_dataset_size 128 \
     --save_strategy "steps" \
     --save_steps 500 \
     --save_total_limit 1 \
